132316251497ac09d3dde83b66e053ac638d72d3
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-08-09 08:34:05 -0700

    Formik form set up, axios in progress

--- client/src/UserForm.js
+++ client/src/UserForm.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { Form, Field, withFormik } from 'formik';
+import axios from "axios";
+import * as Yup from 'yup';
 
 
 
@@ -10,10 +13,45 @@ class UserForm extends React.Component {
         return (
             <div>
                 <h2>User Form Goes here</h2>
+                <Form> 
+                    <Field type="text" name="username" placeholder="Username" />
+                    {/* {touched.name && errors.name && (
+                        <p className="error">{errors.name}</p>
+                    )} */}
+                    <Field type="password" name="password" placeholder="Password" />
+                    {/* {touched.password && errors.password && (
+                        <p className="error">{errors.password}</p>
+                    )} */}
+                     <button type="submit">Submit</button>
+                </Form>
+
             </div>
         )
     }    
 }    
 
+const FormikUserForm = withFormik({
+    mapPropsToValues( { username, password } ) {
+        return {
+            username: username || "",
+            password: password || "",
+        };
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required("Username is required"),
+        password: Yup.string().min(6,"Password must be at least 6 characters").required("Password is required"),
+    }),
 
-export default UserForm;
+    handleSubmit(values, { setStatus }) {
+        axios
+            .post("http://localhost:5000/api/register", values)
+            // .then(res => console.log("res", res.data.name))
+            .then(res => {
+                console.log("response", res)
+                // setStatus(res.data);
+            })
+            .catch(err => console.log("Error", err));
+    }
+})(UserForm);
+
+export default FormikUserForm;
